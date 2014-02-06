3f3aa235448e68a59546df213e0c9b57549ea53e
Pepe Doval <pepellou@gmail.com>
2014-02-06 20:19:59 +0100

    Preset in separate file

---
+++ presets.js
@@ -0,0 +1,10 @@
+var predefined_textures = {
+    water: {
+        bg: { r: 50, g: 100, b: 200 },
+        fg: { r: 7, g: 28, b: 10 }
+    },
+    fire: {
+        bg: { r: 255, g: 0, b: 0 },
+        fg: { r: 247, g: 228, b: 0 }
+    }
+};
